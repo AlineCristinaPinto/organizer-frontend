93c8c85b5fc237ac95138aa7615646d02ab249b2
Luiz Gustavo <lgthxt@gmail.com>
2018-11-22 18:23:35 -0200

    Updating ajaxActions.js's communication to use JSON

--- organizer/app/actions/ajaxActions.js
+++ organizer/app/actions/ajaxActions.js
@@ -2,8 +2,9 @@ export const ajaxRequest = (data,process) => {
     let url = "http://192.168.0.22:8080/1/ServletTeste";
     fetch(url, {
         method: "POST",
-        headers: {"Content-Type":"application/x-www-form-urlencoded",
-                 "User-Type":"mobile-client"}, 
+        headers: {Accept:"application/json",
+                  "Content-Type":"application/json",
+                  "User-Type":"mobile-client"}, 
         body: JSON.stringify(data)
     })
     .then((response) => response.json())
